51cfe79e62e8b1855ef263175eefe46ebbae9c64
GitHub Action <action@github.com>
2025-09-01 09:48:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/zy1263188600/keyswitch/MyBundle.kt
+++ src/main/kotlin/com/github/zy1263188600/keyswitch/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zy1263188600.keyswitch
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zy1263188600/keyswitch/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/zy1263188600/keyswitch/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.zy1263188600.keyswitch.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project

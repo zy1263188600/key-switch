19cef04583489b6114f97b4393250bf11777c755
zengyi <824689624@qq.com>
2025-09-09 16:19:00 +0800

    修复BUGCannot init component state (componentName=InputMethodSettings, componentClass=SettingsState) [Plugin: com.github.zy1263188600.keyswitch]

--- src/main/java/view/SettingsState.java
+++ src/main/java/view/SettingsState.java
@@ -3,33 +3,35 @@ package view;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
-import com.intellij.openapi.components.Storage; 
-import org.jetbrains.annotations.NotNull; 
+import com.intellij.openapi.components.Storage;
+import com.intellij.ui.Gray;
+import com.intellij.ui.JBColor;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 
 @State(
-    name = "InputMethodSettings",
-    storages = @Storage("InputMethodSettings.xml") 
+        name = "InputMethodSettings",
+        storages = @Storage("InputMethodSettings.xml")
 )
 public class SettingsState implements PersistentStateComponent<SettingsState> {
     // 输入法切换策略
     public String strategyClass = "UIAutomationSwitcher";
 
     // 新增光标颜色配置
-    public Color englishCursorColor;
-    public Color chineseCursorColor;
- 
+    public Color englishCursorColor = JBColor.RED;
+    public Color chineseCursorColor = JBColor.BLUE;
+
     @Nullable
     @Override
     public SettingsState getState() {
         return this;
     }
- 
+
     @Override
     public void loadState(@NotNull SettingsState state) {
-        this.strategyClass  = state.strategyClass; 
+        this.strategyClass = state.strategyClass;
     }
 
     public static SettingsState getInstance() {

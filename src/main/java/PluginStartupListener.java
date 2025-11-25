1b98c0c5b98ff3aa5f68c9a94f3b7247588e2238
zengyi <zengyi@fourjoys.cn>
2025-11-25 10:03:23 +0800

    右下角弹窗使用Action实现

--- src/main/java/PluginStartupListener.java
+++ src/main/java/PluginStartupListener.java
@@ -2,6 +2,8 @@ import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
@@ -20,30 +22,26 @@ public class PluginStartupListener implements com.intellij.openapi.startup.Proje
     @Override
     public @Nullable Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
         if (isFirstRun) {
-            // 在EDT线程中执行UI操作
             ApplicationManager.getApplication().invokeLater(() -> {
-                // 创建通知并设置点击监听器
-                Notification notification = new Notification("key-switch",
+                Notification notification = new Notification(
+                        "key-switch",
                         "欢迎使用 key-switch 智能输入法自动切换工具",
-                        "<html>这个工具旨在帮助您更高效地进行编程，不再频繁切换中英文输入状态" +
-                                "<div style='margin-top: px;'>" +
-                                "<a href=\"https://github.com/zy1263188600/key-switch\">⭐star支持</a>" +
-                                "<span style='margin: 0 px;'>或</span>" +
-                                "<a href=\"https://plugins.jetbrains.com/plugin/28418-key-switch\">⭐ 五星好评</a>" +
-                                "</div></html>",
-                        NotificationType.INFORMATION);
+                        "这个工具旨在帮助您更高效地进行编程，不再频繁切换中英文输入状态",
+                        NotificationType.INFORMATION
+                );
 
-                // 设置通知监听器，处理链接点击
-                notification.setListener((notification1, event) -> {
-                    if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-                        try {
-                            String url = event.getDescription();
-                            Desktop.getDesktop().browse(new URI(url));
-                            // 关闭通知
-                            notification1.expire();
-                        } catch (Exception e) {
-                            // 忽略打开浏览器失败的情况
-                        }
+                // 添加操作按钮
+                notification.addAction(new AnAction("⭐star支持") {
+                    @Override
+                    public void actionPerformed(@NotNull AnActionEvent e) {
+                        openUrl("https://github.com/zy1263188600/key-switch", notification);
+                    }
+                });
+
+                notification.addAction(new AnAction("⭐ 五星好评") {
+                    @Override
+                    public void actionPerformed(@NotNull AnActionEvent e) {
+                        openUrl("https://plugins.jetbrains.com/plugin/28418-key-switch", notification);
                     }
                 });
 
@@ -52,5 +50,13 @@ public class PluginStartupListener implements com.intellij.openapi.startup.Proje
             });
         }
         return null;
+    }
+
+    private void openUrl(String url, Notification notification) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+            notification.expire();
+        } catch (Exception ignored) {
+        }
     }
 }

a5436a0f6586fdc8d446c4b24c8aae302c126da0
zengyi <zengyi@fourjoys.cn>
2025-11-26 17:00:41 +0800

    支持以下UI场景识别和设置场景默认输入法

--- src/main/java/editoraction/CursorTrackerService.java
+++ src/main/java/editoraction/CursorTrackerService.java
@@ -2,43 +2,53 @@ package editoraction;
 
 import com.intellij.openapi.CompositeDisposable;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.Service;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.*;
 import com.intellij.openapi.editor.event.*;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.source.javadoc.PsiDocTokenImpl;
+import com.intellij.psi.javadoc.PsiDocToken;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.Alarm;
 import enums.InputState;
 import inputmethod.cursor.CursorHandle;
 import inputmethod.switcher.InputMethodSwitcher;
 import org.jetbrains.annotations.NotNull;
 import utlis.LogUtil;
+import state.SettingsState;
 
-import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.util.Map;
-import java.util.Timer;
+import java.util.Objects;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicReference;
+
+import static editoraction.FocusHandel.*;
 
 @Service(Service.Level.APP)
 public final class CursorTrackerService implements Disposable {
-    private static final long EVENT_THRESHOLD = 100; // 事件过滤阈值（毫秒）
+    //    private static final long EVENT_THRESHOLD = 100; // 事件过滤阈值（毫秒）
     private static final Character.UnicodeBlock[] CHINESE_BLOCKS = { // 中文相关Unicode区块
             Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS,
             Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION,
             Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
     };
     private final CompositeDisposable composite = new CompositeDisposable();
-    private final Map<Editor, Long> lastInputTimeMap = new ConcurrentHashMap<>();
+    //    private final Map<Editor, Long> lastInputTimeMap = new ConcurrentHashMap<>();
     private final Map<Editor, Boolean> selectionStateMap = new ConcurrentHashMap<>();
 
     public CursorTrackerService() {
         EditorEventMulticaster editorEventMulticaster = EditorFactory.getInstance().getEventMulticaster();
-        setupDocumentListener(editorEventMulticaster);
+//        setupDocumentListener(editorEventMulticaster);
         setupCaretListener(editorEventMulticaster);
         setupMouseListener(editorEventMulticaster);
         setupEditorLifecycleListener();
+        setupFocusListener();
 
         Disposer.register(this, this::disposeResources);
     }
@@ -50,38 +60,95 @@ public final class CursorTrackerService implements Disposable {
 
     private void disposeResources() {
         composite.dispose();
-        lastInputTimeMap.clear();
+//        lastInputTimeMap.clear();
         selectionStateMap.clear();
     }
 
-    private void setupDocumentListener(EditorEventMulticaster multicaster) {
-        multicaster.addDocumentListener(new DocumentListener() {
-            @Override
-            public void documentChanged(@NotNull DocumentEvent event) {
-                updateLastInputTime(event.getDocument());
-            }
-        }, composite);
-    }
-
-    private void updateLastInputTime(Document document) {
-        long currentTime = System.currentTimeMillis();
-        for (Editor editor : EditorFactory.getInstance().getEditors(document)) {
-            lastInputTimeMap.put(editor, currentTime);
-        }
-    }
+//    private void setupDocumentListener(EditorEventMulticaster multicaster) {
+//        multicaster.addDocumentListener(new DocumentListener() {
+//            //文档变化
+//            @Override
+//            public void documentChanged(@NotNull DocumentEvent event) {
+//                updateLastInputTime(event.getDocument());
+//            }
+//        }, composite);
+//    }
 
     private void setupCaretListener(EditorEventMulticaster multicaster) {
         multicaster.addCaretListener(new CaretListener() {
+            //光标变化
             @Override
             public void caretPositionChanged(@NotNull CaretEvent event) {
-                handleCaretMovement(event);
+                handleCaretMovement(event.getEditor());
             }
         }, composite);
     }
 
-    private void handleCaretMovement(CaretEvent event) {
+    private void setupMouseListener(EditorEventMulticaster multicaster) {
+        multicaster.addEditorMouseListener(new EditorMouseListener() {
+            //鼠标按下
+            @Override
+            public void mousePressed(@NotNull EditorMouseEvent e) {
+                if (e.getMouseEvent().getButton() == MouseEvent.BUTTON1) {
+                    selectionStateMap.put(e.getEditor(), true);
+                }
+            }
+
+            //鼠标释放
+            @Override
+            public void mouseReleased(@NotNull EditorMouseEvent e) {
+                if (e.getMouseEvent().getButton() != MouseEvent.BUTTON1) return;
+
+                Editor editor = e.getEditor();
+                if (selectionStateMap.remove(editor) != null &&
+                        editor.getSelectionModel().hasSelection()) {
+                    switchInputOnChar(editor);
+                }
+            }
+        }, composite);
+    }
+
+    //编辑器变化监听
+    private void setupEditorLifecycleListener() {
+        EditorFactory.getInstance().addEditorFactoryListener(new EditorFactoryListener() {
+            //关闭编辑器时
+            @Override
+            public void editorReleased(@NotNull EditorFactoryEvent event) {
+                Editor editor = event.getEditor();
+//                lastInputTimeMap.remove(editor);
+                selectionStateMap.remove(editor);
+            }
+        }, composite);
+    }
+
+    //焦点变化监听器
+    private void setupFocusListener() {
+        KeyboardFocusManager.getCurrentKeyboardFocusManager()
+                .addPropertyChangeListener("focusOwner", e -> {
+//                    LogUtil.info(getFocusedComponentHierarchy());
+                    FocusArea focusArea = detectFocusArea();
+                    if (focusArea == FocusHandel.FocusArea.EDITOR) {
+                        LogUtil.info(focusArea.name);
+                        switchInputOnState(SettingsState.getInstance().editorInputState);
+                    } else if (focusArea == FocusHandel.FocusArea.RENAME_DIALOG) {
+                        LogUtil.info(focusArea.name);
+                        //这里会弹出新的窗体导致windows的焦点切换所以暂定需要延迟进行切换 窗体构建完成切换是最准确的
+                        Alarm alarm = new Alarm(Alarm.ThreadToUse.SWING_THREAD, this);
+                        alarm.addRequest(() -> switchInputOnState(SettingsState.getInstance().renameDialogInputState), 1);
+                    } else if (focusArea == FocusHandel.FocusArea.TERMINAL) {
+                        LogUtil.info(focusArea.name);
+                        switchInputOnState(SettingsState.getInstance().terminalInputState);
+                    } else if (focusArea == FocusHandel.FocusArea.SEARCH) {
+                        LogUtil.info(focusArea.name);
+                        switchInputOnState(SettingsState.getInstance().searchInputState);
+
+                    }
+                });
+    }
+
+
+    private void handleCaretMovement(Editor editor) {
         //fix 延迟执行等待selectionModel.hasSelection()、selectionModel.getSelectionStart()/getSelectionEnd()更新最新的光标位置状态
-        Editor editor = event.getEditor();
         Alarm alarm = new Alarm(Alarm.ThreadToUse.SWING_THREAD, this);
         alarm.addRequest(() -> {
             if (editor.isDisposed()) {
@@ -91,10 +158,61 @@ public final class CursorTrackerService implements Disposable {
                 LogUtil.debug(" 检测到文本选择，跳过输入法切换");
                 return;
             }
-            if (shouldProcessMovement(editor)) {
-                switchInputMethodBasedOnChar(editor);
+//            InputState inputState = detectAndPrintElementType(editor);
+//            if (inputState != null) {
+//                //如果在特殊区域则按配置进行输入法切换
+//                switchInputOnState(inputState);
+//            } else {
+                //非特殊区域才进行前一个字符判断是否输入法切换
+                switchInputOnChar(editor);
+//            }
+        }, 1);
+    }
+
+    public static InputState detectAndPrintElementType(Editor editor) {
+        Project project = editor.getProject();
+        if (project == null || project.isDisposed()) {
+            LogUtil.debug("Project 不可用或已释放");
+            return null;
+        }
+        PsiFile psiFile = PsiDocumentManager.getInstance(project)
+                .getPsiFile(editor.getDocument());
+        if (psiFile == null) {
+            LogUtil.debug("当前不在可解析的文件中");
+            return null;
+        }
+
+        int offset = editor.getCaretModel().getOffset() > 0 ? editor.getCaretModel().getOffset() - 1 : 0;
+
+        PsiElement element = psiFile.findElementAt(offset);
+        if (element == null) {
+            LogUtil.debug("无法确定当前Psi元素类型");
+            return null;
+        }
+        System.out.println("未知元素Element:  " + element + ", Class: " + element.getClass());
+        switch (element) {
+            case PsiComment psiComment -> {
+                System.out.println("普通注释:" + psiComment.getText());
+                return InputState.CHINESE;
             }
-        }, 10);
+            case PsiDocToken psiDocToken -> {
+                System.out.println("文档注释:" + psiDocToken.getText()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              );
+                return InputState.CHINESE;
+            }
+
+            case PsiWhiteSpace psiWhiteSpace -> {
+                System.out.println("空白区域:" + psiWhiteSpace.getText());
+                return null;
+            }
+            case PsiJavaToken psiJavaToken -> {
+                System.out.println("java关键字:" + psiJavaToken.getText());
+                return null;
+            }
+            default -> {
+//                System.out.println("未知元素Element:  " + element + ", Class: " + element.getClass());
+                return null;
+            }
+        }
     }
 
     private boolean isWithinSelectionBoundary(Editor editor) {
@@ -128,66 +246,27 @@ public final class CursorTrackerService implements Disposable {
 
     }
 
-    private boolean shouldProcessMovement(Editor editor) {
-        long lastInputTime = lastInputTimeMap.getOrDefault(editor, 0L);
-        long elapsed = System.currentTimeMillis() - lastInputTime;
-        if (elapsed > EVENT_THRESHOLD) {
-            return true;
+    private boolean switchInputOnState(InputState state) {
+        InputState currentMode = InputMethodSwitcher.getCurrentMode();
+        LogUtil.debug("当前输入法状态：" + currentMode);
+        boolean b = !state.equals(currentMode);
+        if (b) {
+            LogUtil.info(" 触发输入法切换");
+            InputMethodSwitcher.change();
         }
-        LogUtil.debug(" 抛弃光标移动事件（时间阈值）");
-        return false;
+        return b;
     }
 
-    private void setupMouseListener(EditorEventMulticaster multicaster) {
-        multicaster.addEditorMouseListener(new EditorMouseListener() {
-            @Override
-            public void mousePressed(@NotNull EditorMouseEvent e) {
-                if (e.getMouseEvent().getButton() == MouseEvent.BUTTON1) {
-                    selectionStateMap.put(e.getEditor(), true);
-                }
-            }
-
-            @Override
-            public void mouseReleased(@NotNull EditorMouseEvent e) {
-                if (e.getMouseEvent().getButton() != MouseEvent.BUTTON1) return;
-
-                Editor editor = e.getEditor();
-                if (selectionStateMap.remove(editor) != null &&
-                        editor.getSelectionModel().hasSelection()) {
-                    switchInputMethodBasedOnChar(editor);
-                }
-            }
-        }, composite);
-    }
-
-    private void setupEditorLifecycleListener() {
-        EditorFactory.getInstance().addEditorFactoryListener(new EditorFactoryListener() {
-            @Override
-            public void editorReleased(@NotNull EditorFactoryEvent event) {
-                Editor editor = event.getEditor();
-                lastInputTimeMap.remove(editor);
-                selectionStateMap.remove(editor);
-            }
-        }, composite);
-    }
-
-
-    private void switchInputMethodBasedOnChar(Editor editor) {
+    private void switchInputOnChar(Editor editor) {
         int offset = editor.getCaretModel().getOffset();
         String prefix = getPrefixText(editor, offset, 1);
-
         if (prefix.isEmpty()) {
             return;
         }
-
         InputState prevCharState = getCharacterState(prefix.charAt(0));
-        InputState currentMode = InputMethodSwitcher.getCurrentMode();
-//        LogUtil.info("前一个字符：" + prefix.charAt(0));
-//        LogUtil.info("前一个字符状态：" + prevCharState);
-//        LogUtil.info("当前输入法状态：" + currentMode);
-        if (!prevCharState.equals(currentMode)) {
-            LogUtil.info(" 触发输入法切换");
-            InputMethodSwitcher.change();
+        LogUtil.debug("前一个字符：" + prefix.charAt(0));
+        LogUtil.debug("前一个字符状态：" + prevCharState);
+        if (switchInputOnState(prevCharState)) {
             CursorHandle.change(editor, prevCharState);
         }
     }
--- src/main/java/utlis/LogUtil.java
+++ src/main/java/utlis/LogUtil.java
@@ -11,7 +11,7 @@ public class LogUtil {
 
     public static void info(String message, Class<?> clazz) {
         // 输出到控制台
-        System.out.println("[info] [" + System.currentTimeMillis() + "] " + message);
+//        System.out.println("[info] [" + System.currentTimeMillis() + "] " + message);
         // 输出到 IDEA 日志系统 
         Logger.getInstance(clazz).info(message);
     }
@@ -22,6 +22,7 @@ public class LogUtil {
     }
 
     public static void debug(String message, Class<?> clazz) {
+//        System.out.println("[debug] [" + System.currentTimeMillis() + "] " + message);
         // 输出到 IDEA 日志系统
         Logger.getInstance(clazz).debug(message);
     }

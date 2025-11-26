6c4e4369dc764bd38406e8eb5dea9f542cc1ebac
zengyi <zengyi@fourjoys.cn>
2025-11-26 17:11:44 +0800

    弹窗快捷跳转至本插件设置页面

--- src/main/java/PluginStartupListener.java
+++ src/main/java/PluginStartupListener.java
@@ -5,6 +5,7 @@ import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import kotlin.Unit;
@@ -24,14 +25,24 @@ public class PluginStartupListener implements com.intellij.openapi.startup.Proje
         if (isFirstRun) {
             ApplicationManager.getApplication().invokeLater(() -> {
                 Notification notification = new Notification(
-                        "key-switch",
+                        "Key-Switch",
                         "欢迎使用 key-switch 智能输入法自动切换工具",
                         "这个工具旨在帮助您更高效地进行编程，不再频繁切换中英文输入状态",
                         NotificationType.INFORMATION
                 );
 
                 // 添加操作按钮
-                notification.addAction(new AnAction("⭐star支持") {
+                notification.addAction(new AnAction("插件设置") {
+                    @Override
+                    public void actionPerformed(@NotNull AnActionEvent e) {
+                        ShowSettingsUtil.getInstance().showSettingsDialog(
+                                e.getProject(),
+                                "Key-Switch" // 必须与plugin.xml 中定义的<name>完全一致
+                        );
+                    }
+                });
+
+                notification.addAction(new AnAction("⭐ Star支持") {
                     @Override
                     public void actionPerformed(@NotNull AnActionEvent e) {
                         openUrl("https://github.com/zy1263188600/key-switch", notification);
